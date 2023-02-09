aee4fb8f83815e3962ede4d75c1eb274358e0565
Dat Thai Hoang Tien <ext-dat.thai@spdigital.sg>
2023-02-09 17:17:11 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { NavBar } from './NavBar'
+import { Info } from './Info'
+
+function App() {
+
+  return (
+    <div className="App">
+      {/*----NAVBAR----*/}
+      <NavBar />
+      {/*----INFO----*/}
+      <Info />
+      {/*----DETAIL----*/}
+      {/*----GRAPH----*/}
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/NavBar/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import MenuIcon from '../assets/icons/menu.svg'
+import NotificationIcon from '../assets/icons/notification.svg'
+import classes from './NavBar.module.scss'
+
+export const NavBar = () => { 
+  return (
+    <nav className={classes.navbar}>
+      <img src={MenuIcon} className={classes['navbar__icon']} alt="menu icon"/>
+      <div className={classes['navbar__content']}>
+        <p>MyENV</p>
+        <p className={classes['navbar__dropdown']}>Current Location</p>
+      </div>
+      <img src={NotificationIcon} className={classes['navbar__icon']} alt="notification icon"/>
+    </nav>
+  )
+}
